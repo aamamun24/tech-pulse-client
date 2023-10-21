b1cf72fce5db9627d7a6b225f9db538437e5076b
aamamun24 <bdmamaun100@gmail.com>
2023-10-21 23:57:16 +0600

    cart problem solved

--- src/pages/Cart/CartData.jsx
+++ src/pages/Cart/CartData.jsx
@@ -39,16 +39,26 @@ const CartData = ({ cart, carts, setCarts }) => {
 
 
     return (
-        <div className="max-w-2xl mx-auto bg-white p-4 shadow-md rounded-lg mb-4 flex flex-col md:flex-row gap-6 justify-between items-center">
-            <div className="flex items-center">
-                <img src={image} alt={name} className="w-16 h-16 object-cover rounded-lg" />
-                <div className="ml-4">
-                    <h2 className="text-lg font-semibold">{name}</h2>
-                    <p className="text-gray-600">{type}</p>
-                </div>
-            </div>
-            <button onClick={() => { handleDelete(_id) }} className="bg-red-600 w-full md:w-auto hover:bg-red-800 text-white py-2 px-4 rounded focus:outline-none">Delete</button>
-        </div>
+        <>
+            {
+                cart ?
+                    <div className="max-w-2xl mx-auto bg-white p-4 shadow-md rounded-lg mb-4 flex flex-col md:flex-row gap-6 justify-between items-center">
+                        <div className="flex items-center">
+                            <img src={image} alt={name} className="w-16 h-16 object-cover rounded-lg" />
+                            <div className="ml-4">
+                                <h2 className="text-lg font-semibold">{name}</h2>
+                                <p className="text-gray-600">{type}</p>
+                            </div>
+                        </div>
+                        <button onClick={() => { handleDelete(_id) }} className="bg-red-600 w-full md:w-auto hover:bg-red-800 text-white py-2 px-4 rounded focus:outline-none">Delete</button>
+                    </div>
+                    :
+                    <div>
+                        <h2 className='text-3xl font-bold text-center'>Your are not added any product</h2>
+                    </div>
+            }
+
+        </>
     );
 };
 
